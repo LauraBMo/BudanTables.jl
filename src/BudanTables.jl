205ab65ce8012b75ec397da55eeeaca71716b49c
Laura Brustenga i Moncusí <brust@math.ku.dk>
2022-02-07 17:03:00 +0100

    use Requires load PolynomialRoots.roots on demand

--- src/BudanTables.jl
+++ src/BudanTables.jl
@@ -1,11 +1,15 @@
 module BudanTables
 
 using DocStringExtensions:SIGNATURES
-using RecipesBase
-using Polynomials:roots, derivative, degree # Polynomial type, roots function
-# using Plots:Shape
+using Requires
 
-const _ROOTS = Ref{Function}(roots)
+using RecipesBase
+using Polynomials # Polynomial type, roots function
+# using Plots
+
+export BudanTable, budantable, set_getting_roots
+
+const _ROOTS = Ref{Function}(Polynomials.roots)
 
 """
 $(SIGNATURES)
@@ -33,10 +37,17 @@ function set_getting_roots(f)
     _ROOTS[] = f
 end
 
+function __init__()
+        @require PolynomialRoots="3a141323-8675-5d76-9d11-e1df1406c778" begin
+            # Use Polynomials api for poly arith. and PR's roots for roots
+            _BigFloat(A::AbstractArray) = BigFloat.(A)
+            set_getting_roots(PolynomialRoots.roots∘_BigFloat∘Polynomials.coeffs)
+        end
+end
+
 include("aux_code.jl")
 
 @userplot BudanTable
-export BudanTable, budantable
 
 """
 $(SIGNATURES)
